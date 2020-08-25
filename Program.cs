886ec23e89c2487188601273c0712c0944a02f06
Tao141516 <68211666+Tao141516@users.noreply.github.com>
2020-08-25 21:55:41 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace oop_6115261020_w02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room r434 = new Room("434", "434", 4, 4, "Computer");
+            Console.WriteLine(r434);
+            Building b4 = new Building("4 ", " Computer ", " 5.335 ", " 532.321 ");
+            Console.WriteLine(b4);
+            Subject th = new Subject("34511 ", " thai ", " 2 ", " 2 ", " 2 ");
+            Console.WriteLine(th);
+            Lecturer tc = new Lecturer("Nantida ", " Rodaumpan ", " Teacher ");
+            Console.WriteLine(tc);
+            Section tt = new Section("322 ", " 01 ", " Computer ", " T.tao ", " 01 Oct ", " 09.30 ", " 12.00 ");
+            Console.WriteLine(tt);
+            Programm aa = new Programm("Com ", " Bachelor degree ");
+            Console.WriteLine(aa);
+        }
+    }
+}
